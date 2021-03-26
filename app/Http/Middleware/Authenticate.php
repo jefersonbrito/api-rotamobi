1428020b7513217c462df62bffb6fadd2c9b582f
Jeferson Brito <jeferson@jefersonbrito.com.br>
2021-03-25 22:16:45 -0300

    Ajustes no API para coleta da posição do motorista

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -5,8 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Contracts\Auth\Factory as Auth;
 
-class Authenticate {
-
+class Authenticate
+{
     /**
      * The authentication guard factory instance.
      *
@@ -20,7 +20,8 @@ class Authenticate {
      * @param  \Illuminate\Contracts\Auth\Factory  $auth
      * @return void
      */
-    public function __construct(Auth $auth) {
+    public function __construct(Auth $auth)
+    {
         $this->auth = $auth;
     }
 
@@ -32,31 +33,13 @@ class Authenticate {
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null) {
-        $headers = [
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age' => '86400',
-            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'
-        ];
-
-        if ($request->isMethod('OPTIONS')) {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+    public function handle($request, Closure $next, $guard = null)
+    {
+        
+        if ($this->auth->guard($guard)->guest()) {
+            return response('Unauthorized.', 401);
         }
 
-        $response = $next($request);
-        foreach ($headers as $key => $value) {
-            $response->header($key, $value);
-        }
-        
-//        if ($this->auth->guard($guard)->guest()) {
-//            return response('Unauthorized.', 401);
-//        }
-
-//        return $next($request);
-        
-         return $response;
+        return $next($request);
     }
-
 }
